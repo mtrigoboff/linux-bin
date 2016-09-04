2dd336c0e76751ec0def1aad2f8d0ea8f7c08108
Michael Trigoboff <michael.trigoboff@pcc.edu>
2016-09-04 11:32:14 -0700

    changed tarzcodefiles to tarzcode

--- create_makefile.py
+++ create_makefile.py
@@ -14,14 +14,14 @@ tabWidth =	4
 header = 'include ~michael.trigoboff/bin/makeflags_g++\n'
 footer = ('.PHONY: x', 'x:', '# clean the directory', 'rm -f *.o')
 
-def createMakefile(projectPath):
+def createMakefile(projectDirPath):
 
 	# collect .cpp files and their .h dependencies into cppFiles
 	targets = []
-	for fileName in os.listdir(projectPath):
+	for fileName in os.listdir(projectDirPath):
 		fileNameBase, fileNameExt = os.path.splitext(fileName)
 		if fileNameExt == '.cpp':
-			filePath = os.path.join(projectPath, fileName)
+			filePath = os.path.join(projectDirPath, fileName)
 			cppFile = open(filePath, mode='r')
 			lines = cppFile.readlines()
 			cppFile.close()
@@ -29,14 +29,14 @@ def createMakefile(projectPath):
 			for line in lines:
 				if line.startswith('#include "'):
 					hdrFiles.append(line.split('"')[1])
-			targetNameSuffix = '.o' if not fileNameBase.startswith(appTargetNameStart) else ''
+			targetNameSuffix = '.o' if not fileNameBase == appTargetName else ''
 			targetName = fileNameBase + targetNameSuffix
 			targets.append(Target(targetName, len(targetName) + 1, hdrFiles))
 			# +1 for ':' delimiter after targetName in makefile
 
 	# gather .o files needed for asgmt0[1234] rule, add them to dependencies for app
-	assert targets[0].name.startswith(appTargetNameStart)
-	oFiles = [target.name for target in targets if not target.name.startswith(appTargetNameStart)]
+	assert targets[0].name == appTargetName
+	oFiles = [target.name for target in targets if not target.name == appTargetName]
 	targets[0].dependencies = oFiles + targets[0].dependencies
 
 	# compute max number of tabs needed for any target
@@ -51,11 +51,11 @@ def createMakefile(projectPath):
 		target.nTabs = nTabsMax - int(target.nameLgth / 4)
 
 	# write makefile
-	makefile = open(os.path.join(projectPath, 'makefile'), 'w')
+	makefile = open(os.path.join(projectDirPath, 'makefile'), 'w')
 	print(header,										file=makefile, sep='')
 	for target in targets:
 
-		# print name followed by correct number of tabs
+		# print name followed by ':' and correct number of tabs
 		print(target.name, ':', '\t' * target.nTabs,	file=makefile, end='', sep='')
 
 		# print dependencies
@@ -67,9 +67,8 @@ def createMakefile(projectPath):
 				print(' ',								file=makefile, end='', sep='')
 			print(dependent,							file=makefile, end='', sep='')
 
-		# print end of line
-		print(											file=makefile,		   sep='')
-		print(											file=makefile,		   sep='')
+		# print 2 newlines
+		print('\n' * 2									file=makefile, end='', sep='')
 
 	print(footer[0],									file=makefile,		   sep='')
 	print(footer[1],									file=makefile, end='', sep='')
@@ -81,17 +80,16 @@ def createMakefile(projectPath):
 
 	makefile.close()
 
-if len(sys.argv) < 3:
-	print('need to specify folder path, app target name.startswith()')
-	sys.exit()
-
 # when invoked from the command line
 if __name__ == '__main__':
-	projectPath = sys.argv[1]
-	appTargetNameStart = sys.argv[2]
-	print('makefiles: ', projectPath, appTargetNameStart)
-	if os.path.isfile(projectPath):
+	if len(sys.argv) < 3:
+		print('command line args: projectDirectoryPath appTargetName')
+		sys.exit()
+	projectDirPath = sys.argv[1]
+	appTargetName = sys.argv[2]
+	print('makefiles: ', projectDirPath, appTargetName)
+	if os.path.isfile(projectDirPath):
 		print('not a directory')
 		sys.exit()
-	print(projectPath)
-	createMakefile(projectPath)
+	print(projectDirPath)
+	createMakefile(projectDirPath)
